9be0562e7823e95f18c036dd41a102c79f2da008
raneenana <17773439@qq.com>
2019-04-25 15:01:10 +0800

    “updata address

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -25,7 +25,7 @@ module.exports = {
   serviceWorker: true,
   theme: 'vue',
   themeConfig: {
-    repo: 'jasonandjay/study',
+    repo: 'raneenana/study',
 
     docsDir: 'src',
     editLinks: true,
@@ -33,7 +33,7 @@ module.exports = {
     locales: {
       '/': {
         label: '简体中文',
-        repo: 'jasonandjay/study',
+        repo: 'raneenana/study',
       
         selectText: '选择语言',
         editLinkText: '帮助我们改进内容',
